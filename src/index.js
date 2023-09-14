eadcc2a8ca4fdf1d58935105403d0a16c8676f38
iVladyuser <korol.v09@gmail.com>
2023-09-14 17:53:07 +0200

    second commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,58 @@
-import { Loading } from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 import SimpleLightbox from 'simplelightbox';
 
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import ref from './js/refs';
-
-const { gallery} = ref;
-
+import  refs  from './js/refs';
 import { fetchAnimals } from './js/pixabay-api';
-import {createMarkUP } from './js/createMarkUP';
+import { createMarkUP } from './js/createMarkUP';
 
-fetchAnimals()
-  .then(data => gallery.insertAdjacentHTML('beforeend', createMarkUP(data.hits)))
+const { form, gallery, btnLoadMore } = refs;
+
+const paramsForNotify = {
+    position: 'center-center',
+    timeout: 4000,
+    width: '500px',
+    fontSize: '22px'
+};
+
+const perPage = 40;
+let page = 1;
+let keyOfPhoto = '';
+
+form.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+
+  gallery.innerHTML = '';
+  page = 1;
+  const { searchQuery } = event.currentTarget.elements;
+  keyOfPhoto = searchQuery.value
+    .trim()
+    .toLowerCase()
+    .split(' ')
+    .join('+');
+	console.log(keyOfPhoto);
+
+	if (keyOfPhoto === '') {
+		Notify.info ('Sorry, there are no images matching your search query. Please try again.', paramsForNotify);
+		return
+
+	}
+}
+
+fetchAnimals(keyOfPhoto, page, perPage)
+  .then(data => {
+    const arr = data.hits;
+	console.log(data.hits);
+	if (data.totalHits === 0) {
+		Notify.failure('Sorry, there are no images matching your search query. Please try again.', paramsForNotify);
+	} else {
+		Notify.info(`Hooray! We found ${data.totalHits} images.`, paramsForNotify);
+	
+    createMarkUP(arr);
+	}
+  })
+
   .catch(error => console.log(error));
-
-
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -6,5 +6,5 @@ const KEY = '39440146-e742bf3c7a957201e6286a658';
 export async function fetchAnimals(q, page, perPage) {
   const url = `${URL}?key=${KEY}&q=${q}&page=${page}&per_page=${perPage}&image_type=photo&orientation=horizontal&safesearch=true`;
   const response = await axios.get(url);
-  return response.data;          
-};
+  return response.data;
+}
