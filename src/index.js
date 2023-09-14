ece0d17f00b9ffd70c0a014d59c0a8db0e1f5190
iVladyuser <korol.v09@gmail.com>
2023-09-14 22:26:41 +0200

    new commit

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
+import './style.css'
 import SimpleLightbox from 'simplelightbox';
 
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import  refs  from './js/refs';
+import refs from './js/refs';
 import { fetchAnimals } from './js/pixabay-api';
 import { createMarkUP } from './js/createMarkUP';
+import { onFetchError } from './js/showError';
 
 const { form, gallery, btnLoadMore } = refs;
 
 const paramsForNotify = {
-    position: 'center-center',
-    timeout: 4000,
-    width: '500px',
-    fontSize: '22px'
+  position: 'center-center',
+  timeout: 1500,
+  width: '400px',
+  fontSize: '22px',
 };
 
-const perPage = 40;
+const perPage = 20;
 let page = 1;
 let keyOfPhoto = '';
 
@@ -28,31 +29,50 @@ function onSubmitForm(event) {
   gallery.innerHTML = '';
   page = 1;
   const { searchQuery } = event.currentTarget.elements;
-  keyOfPhoto = searchQuery.value
-    .trim()
-    .toLowerCase()
-    .split(' ')
-    .join('+');
-	console.log(keyOfPhoto);
+  keyOfPhoto = searchQuery.value.trim().toLowerCase().split(' ').join('+');
 
-	if (keyOfPhoto === '') {
-		Notify.info ('Sorry, there are no images matching your search query. Please try again.', paramsForNotify);
-		return
+  fetchAnimals(keyOfPhoto, page, perPage)
+    .then(data => {
+      const arr = data.hits;
 
-	}
+      if (data.totalHits === 0) {
+        Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.',
+          paramsForNotify
+        );
+      } else {
+        Notify.info(
+          `Hooray! We found ${data.totalHits} images.`,
+          paramsForNotify
+        );
+        createMarkUP(arr);
+      }
+	  if (data.totalHits > perPage) {
+		btnLoadMore.hidden = false;
+		window.addEventListener('scroll', showLoadMorePage);
+	};
+    })
+
+    .catch(onFetchError)
+    .finally(() => form.reset());
+
+  if (keyOfPhoto === '') {
+    Notify.info(
+      'Sorry, there are no images matching your search query. Please try again.',
+      paramsForNotify
+    );
+    return;
+  }
 }
 
-fetchAnimals(keyOfPhoto, page, perPage)
-  .then(data => {
-    const arr = data.hits;
-	console.log(data.hits);
-	if (data.totalHits === 0) {
-		Notify.failure('Sorry, there are no images matching your search query. Please try again.', paramsForNotify);
-	} else {
-		Notify.info(`Hooray! We found ${data.totalHits} images.`, paramsForNotify);
-	
-    createMarkUP(arr);
-	}
-  })
+btnLoadMore.addEventListener('click', onload);
 
-  .catch(error => console.log(error));
+function onload() {
+  page += 1;
+  fetchAnimals(keyOfPhoto, page, perPage)
+    .then(data => {
+      const arr = data.hits;
+      createMarkUP(arr);
+    })
+    .catch(onFetchError);
+}
--- src/js/showError.js
+++ src/js/showError.js
@@ -1,20 +1,19 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import ref from './refs';
-// const { selector,  loader, catInfo } = ref;
+// const { loader } = ref;
 
+export function onFetchError(error) {
+//   catInfo.hidden = true;
+//   selector.hidden = true;
+//   loader.classList.replace('loader', 'is-hidden');
 
-// export function onFetchError(error) {
-//     catInfo.hidden = true;
-//         selector.hidden = true;
-//     loader.classList.replace('loader', 'is-hidden');
-
-//     Notify.failure(
-//     'Oops! Something went wrong! Select please another cat breed!',
-//     {
-//         position: 'center-center',
-//         timeout: 5000,
-//         width: '400px',
-//         fontSize: '24px',
-//     }
-//     );
-// }
+  Notify.failure(
+    'Oops! Something went wrong! Select please another cat breed!',
+    {
+      position: 'center-center',
+      timeout: 5000,
+      width: '400px',
+      fontSize: '24px',
+    }
+  );
+}
